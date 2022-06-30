bfbfdef834f5207489f89f658d1ca0e4a8ecab43
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-29 20:22:21 -0500

    user and calorie models

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const calorie = require('./routes/calorie.routes')
+const users = require('./routes/users.routes')
 
 require('dotenv').config()
 
@@ -9,11 +11,14 @@ const app = express()
 //app config
 app.use(cors())
 app.use(express.json())
+app.use('/calorie', calorie)
+app.use('/users', users)
 
 //port and DB configuration
 const DATABASE_CONNECTION = process.env.DATABASE_URL
 const PORT = process.env.PORT || 3000
 
+//mongoose connection
 mongoose
     .connect(DATABASE_CONNECTION, {
         useNewUrlParser: true,
