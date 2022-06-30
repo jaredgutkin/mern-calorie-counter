6b3827902bf7081e5f375ae09ec51993f572e341
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-29 20:08:03 -0500

    connection to server and mongodb

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
 
+//app config
 app.use(cors())
 app.use(express.json())
+
+//port and DB configuration
+const DATABASE_CONNECTION = process.env.DATABASE_URL
+const PORT = process.env.PORT || 3000
+
+mongoose
+    .connect(DATABASE_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+ 
+//connect to port
+app.listen(PORT, (req, res)=>{
+    console.log(`server runnign on port ${PORT}`)
+})
